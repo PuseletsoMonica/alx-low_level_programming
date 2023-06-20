b00c0c227c1a1737d10611bf215904836082fac9
PuseletsoMonica <puseletsomaile@gmail.com>
2023-06-20 10:18:53 -0700

    alphabet 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+int main(void)
+
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
+
+
+
+
