5a46ec1153db68e8a201f19445e7b9e117fc1eeb
PuseletsoMonica <puseletsomaile@gmail.com>
2023-06-20 17:03:26 -0700

    alphabet 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,16 +4,15 @@
  * main - check the code
  * Return: Always 0 (Success)
  */
-int main(void)
-
+void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		putchar(i);
+	_putchar(i);
 	}
-	putchar('\n');
-	return (0);
+	_putchar('\n');
+
 }
 
