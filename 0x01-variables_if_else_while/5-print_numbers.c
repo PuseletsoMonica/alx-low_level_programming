01aaeb853e28acc19cb5a096cbcb689a354b7cb7
PuseletsoMonica <puseletsomaile@gmail.com>
2023-06-21 17:38:28 -0700

    all singe digits

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - print all single digits from 0 to 9
  * Return: Always 0 (Success)
  */
 int main(void)
 {int i;
-	for (i = 0, i < 10, i++)
+	for (i = 0; i < 10; i++)
 	{
 		printf("%d\n", i);
 	}
