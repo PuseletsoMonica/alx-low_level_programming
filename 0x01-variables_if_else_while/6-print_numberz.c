47f2078564c0f5d5336b307e6a8b23ad3c3439cd
PuseletsoMonica <puseletsomaile@gmail.com>
2023-06-21 17:58:18 -0700

    single numbers in putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print all single digits of base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{int i;
+		for (i = 0; < 10; i++)
+		putchar("0" + i);
+
+		putchar ("\n");
+		return (0);
+	}
