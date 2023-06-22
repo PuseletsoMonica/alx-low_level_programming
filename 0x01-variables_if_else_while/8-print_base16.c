0d5176939e21a633b4a562fdfdd74d88f7b32940
PuseletsoMonica <puseletsomaile@gmail.com>
2023-06-21 18:44:51 -0700

    16 lower base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,9 +5,13 @@
  */
 int main(void)
 {int i;
-	for (i = 0; i <= 16, i++)
-		putchar(i);
+		char hexvalues[] = "0123456789abcdef";
 
-	putchar("\n");
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hexvalues[i]);
+	}
+	putchar('\n');
 	return (0);
 }
+
