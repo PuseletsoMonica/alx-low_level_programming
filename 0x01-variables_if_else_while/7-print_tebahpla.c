a87141e356aa86f446bb693c794ea1ac495a702a
PuseletsoMonica <puseletsomaile@gmail.com>
2023-06-21 18:34:41 -0700

    a - z in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,9 +4,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{int i;
-	for (i = 122; i => 97; i++)
-		putchar("%c ", i);
-	putchar("\n")
-		return (0);
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
 }
+
