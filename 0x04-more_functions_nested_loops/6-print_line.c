d6cf78e535fdaa53c84177d6aecaf08b86859cc9
PuseletsoMonica <puseletsomaile@gmail.com>
2023-06-23 18:45:22 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line n times.
+ *
+ * @n: times straight line is printed.
+ * Return: no return.
+ */
+void print_line(int n)
+{if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
+
+
