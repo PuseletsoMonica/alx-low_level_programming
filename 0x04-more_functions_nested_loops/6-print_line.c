cfe23cdcac1153353d651485b3a954f31737c21b
PuseletsoMonica <puseletsomaile@gmail.com>
2023-06-23 18:47:18 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,20 +7,12 @@
  * Return: no return.
  */
 void print_line(int n)
-{if (n <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i;
+{int i;
 
-		for (i = 1; i <= n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
 	}
-
+	_putchar('\n');
 }
-
 
