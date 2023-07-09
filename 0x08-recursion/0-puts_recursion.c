a03b57fd1d3cad76368d766c16891f4adc09dd0e
PuseletsoMonica <puseletsomaile@gmail.com>
2023-07-09 07:34:37 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/*
+/**
  * _puts_recursion - function like puts();
  * @s: input
  * Return: Always 0 (Success)
@@ -11,7 +11,9 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+
 	else
 		_putchar('\n');
 }
+
 
